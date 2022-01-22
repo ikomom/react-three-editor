7125b23ccead7669c6c92a021b1d91a9e33f8261
ikonon <315793076@qq.com>
2022-01-22 16:26:23 +0800

    lint

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('学习threejs', () => {
+test("学习threejs", () => {
   render(<App />);
   const linkElement = screen.getByText(/学习threejs/i);
   expect(linkElement).toBeInTheDocument();
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import ThreeCompare from "./components/threejs";
-import './App.css'
+import "./App.css";
 
 export default function App() {
   return (
-    <div className={'container m-auto'} style={{height: '100vh'}}>
-      <p className={'text-center font-bold'}>学习threejs</p>
-      <ThreeCompare/>
+    <div className={"container m-auto"} style={{ height: "100vh" }}>
+      <p className={"text-center font-bold"}>学习threejs</p>
+      <ThreeCompare />
     </div>
-  )
+  );
 }
